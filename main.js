97c9fbf1db38f0f450d5567dbe51f9d1717436d1
Anna Erofeeva <b0nes19@yanddex.ru>
2024-03-01 18:53:20 +0300

    update game

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 
- let divHero = document.querySelector('.hero')
+let divHero = document.querySelector('.hero')
 
 function massDoubles() {
     let cardCount = 9;
     let doubles = [];
-  
+
     for (i = 1; i < cardCount; i++) {
         doubles.push(i, i);
     } return doubles;
 }
 
 function MassMixingDoubles(massDoubl) {
- 
-    massDoubl =  massDoubles();
+
+    massDoubl = massDoubles();
     let temp = 0;
-    
+
     for (i = 0; i < massDoubl.length; i++) {
         let j = Math.floor(Math.random() * massDoubl.length)
         temp = massDoubl[i];
@@ -24,65 +24,88 @@ function MassMixingDoubles(massDoubl) {
 
 }
 
-// let mixingMass = MassMixingDoubles();
-
-//Cоздайте функцию, которая будет создавать карточку с номером из массива произвольных чисел.
-//Добавьте событие клика, в котором будут условия и проверки на совпадение карточек.
-
-
-function randomCard (mixin) {
+function randomCard(mixin) {
 
     mixin = MassMixingDoubles();
-    console.log(mixin)
 
-    
-    let carId = [];
+    let cards = [];
 
-    for (i = 0; i < mixin.length; i++ ) {
-        
+    for (i = 0; i < mixin.length; i++) {
+
         let sceneCard = document.createElement('div');
         sceneCard.classList.add('scene');
         divHero.append(sceneCard);
 
         let cardWrap = document.createElement('div');
         cardWrap.classList.add('card');
-        cardWrap.id = mixin[i];
+        cards.push(cardWrap)
         sceneCard.append(cardWrap);
 
         let cardFront = document.createElement('div')
         cardFront.classList.add('card__face', 'card__face--front')
-        // cardFront.textContent = 'Open Me';
         cardWrap.append(cardFront);
+
         let cardBack = document.createElement('div')
         cardBack.classList.add('card__face', 'card__face--back')
         cardBack.textContent = mixin[i];
         cardWrap.append(cardBack);
-
-
-        cardWrap.addEventListener('click', (e) => {
-
-            cardWrap.classList.toggle('is-flipped');
-            setTimeout(function() {
-                cardWrap.classList.remove("is-flipped");
-                }, 1000); //сложность игры увеличивать скорочть уменьшать
-
-                carId.push(cardWrap.id);
-                // console.log(carId)
-                
-                // for (i = 0; i < carId.length; i++) {
-          
-                //     if (carId[i] === carId[i+1]) {             ?? ошибка
-                //         cardWrap.classList.add('flipped');           
-                //        } 
-                    
-                //     }
-        })
-        
-    }
-
-    // return carId;
+    } return cards;
 }
 
-// let card = randomCard();
+let card = randomCard();
 
-randomCard();
+card.forEach((item, i) => {
+  
+    item.addEventListener('click', (e) => {
+
+        //  console.log(item.lastChild.textContent)
+        item.classList.add('is-flipped');
+        setTimeout(function () {
+            item.classList.remove("is-flipped");
+        }, 1000);
+    //   if (item.classList.contains('is-flipped')) {
+    //   }
+        item.id = [i+1];
+        rules ()
+    })
+
+ })
+
+function rules () {
+
+    let mass = [];
+    for (i = 0; i < card.length; i++) {
+       if(card[i].classList.contains('is-flipped'))
+       {
+
+        mass.push(card[i])
+       }
+    }
+     if (mass.length > 1) {
+     for ( i = 0; i < mass.length;i++) {
+
+        // if(mass[i].lastChild.textContent == mass[i+1].lastChild.textContent) {
+        //      mass[i].classList.add('display__none')
+        //  mass[i+1].classList.add('display__none')
+        //  }
+                let first = mass[i];
+                let last = mass[i+1];
+                if(first.lastChild.textContent === last.lastChild.textContent) {
+                    first.classList.add('is-flipped');
+                    setTimeout(function () {
+                        first.classList.add('display__none');
+                    }, 1000);
+                    last.classList.add('is-flipped');
+                    setTimeout(function () {
+                        last.classList.add('display__none');
+                    }, 1000);
+                }
+            console.log('первый текс', first.lastChild.textContent, 'Второй',last.lastChild.textContent)
+     }
+        }
+
+   
+    // console.log(mass)
+}
+
+rules ()
